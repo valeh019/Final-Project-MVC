4c54dfefcf55ea54b4a522e9948a4b9935bdd860
valehh019 <valehrg@code.edu.az>
2024-06-06 03:51:05 +0400

    Final Project

---
+++ Project/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace vbankASP.NET.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
